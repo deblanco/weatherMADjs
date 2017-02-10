2b43989c95845041f7546d540b56467a5cbaa41d
Daniel Blanco Parla <abseotaku@gmail.com>
2017-02-10 13:34:19 +0100

    Update darksky.js

--- modules/darksky/darksky.js
+++ modules/darksky/darksky.js
@@ -22,7 +22,7 @@
             // https://darksky.net/
             $http({
                 method: 'GET',
-                url: 'https://api.darksky.net/forecast/' + APIKEY + '/' + filter + '/?lang=es&units=auto',
+                url: 'http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + APIKEY + '/' + filter + '/?lang=es&units=auto',
                 cache: true,
                 timeout: 3000
             })
